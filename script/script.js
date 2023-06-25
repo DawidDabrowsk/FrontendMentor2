cfa50e3c7bdaf16d68db1a2455a70b80a9bdb35b
Dawid Dabrowski <dawid_d_97@o2.pl>
2023-06-25 15:45:16 +0200

    Card form done with JS

--- script/script.js
+++ script/script.js
@@ -9,19 +9,79 @@ const cardOwner = document.querySelector('.owner')
 const cardDate = document.querySelector('.date')
 const cvcNumber = document.querySelector('.cvc-number')
 const errorCard = document.querySelector('.error-card')
+const errorCvc = document.querySelector('.error-cvc')
+const errorCardName = document.querySelector('.error-card-name')
+const continueBtn = document.querySelector('.continue')
+const mainArea = document.querySelector('.main')
+const popupSuccess = document.querySelector('.popup-success')
 
-const cardNrReg = /^([0-9]{16})$/
-const yearReg = /^([0-9]{4})$/
-const monthReg = /^([0-9]{1-2})$/
-const cvcReg = /^([0-9]{3})$/
+const nameReg = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u
+const cardNrReg = /^([0-9 ]{16,19})+$/g
+const yearReg = /^([0-9]{2,4})$/g
+const monthReg = /^([0-9]{1,2})$/g
+const cvcReg = /^([0-9]{3})$/g
 
-const checkData = () => {
-    if(cardNumberInput.value.length !== 16 || !cardNumberInput.value.match(cardNrReg)) {
-        errorCard.style.visibility = 'visible'
-        console.log(cardNumberInput.value);
-    } else if(monthInput.value.length > 2 || yearInput.value !== 4 || )  {
-    
-    }
+const clearInputs = () => {
+    errorCardName.style.visibility = 'hidden'
+    errorCard.style.visibility = 'hidden'
+    errorCvc.style.visibility = 'hidden'
+    nameInput.classList.remove('wrong-input')
+    cardNumberInput.classList.remove('wrong-input')
+    monthInput.classList.remove('wrong-input')
+    yearInput.classList.remove('wrong-input')
+    cvcInput.classList.remove('wrong-input')
 }
 
+const checkData = () => {
+    clearInputs()
+    if(!nameInput.value.match(nameReg)) {
+        errorCardName.style.visibility = 'visible' 
+        nameInput.classList.add('wrong-input')
+    }
+    if(!cardNumberInput.value.match(cardNrReg)) {
+        errorCard.style.visibility = 'visible' 
+        cardNumberInput.classList.add('wrong-input')
+    }
+    if(!monthInput.value.match(monthReg)) {
+        monthInput.classList.add('wrong-input')
+        errorCvc.style.visibility = 'visible'
+        errorCvc.innerHTML = 'Wrong month format'
+    }
+    if(!yearInput.value.match(yearReg)) {
+        yearInput.classList.add('wrong-input')
+        errorCvc.style.visibility = 'visible'
+        errorCvc.innerHTML = 'Wrong year format'
+    }
+    if(!cvcInput.value.match(cvcReg)) {
+        cvcInput.classList.add('wrong-input')
+        errorCvc.style.visibility = 'visible'
+        errorCvc.innerHTML = 'Wrong CVC number'
+    }
+    if(nameReg.test(nameInput.value) && cardNrReg.test(cardNumberInput.value) && monthReg.test(monthInput.value) && yearReg.test(yearInput.value) && cvcReg.test(cvcInput.value)) {
+        mainArea.style.display = 'none'
+        popupSuccess.style.display = 'block'
+    }
+
+}
+
+const changeCardOwner = () => {
+    cardOwner.textContent = nameInput.value
+}
+
+const changeCardNumber = () => {
+    cardNumber.textContent = cardNumberInput.value
+}
+
+const changeCardData = () => {
+    cardDate.innerHTML = `${monthInput.value}/${yearInput.value}`
+}
+
+const changeCvcNumber = () => {
+    cvcNumber.textContent = cvcInput.value
+}
+
+
 confirmBtn.addEventListener('click', checkData)
+continueBtn.addEventListener('click', () => {
+    location.reload()
+})
